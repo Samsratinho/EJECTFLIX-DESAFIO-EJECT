f8d1c9563a722ad8927efe6aa90920caaa1f40bb
Samuel Silva de Lima <samuclient@gmail.com>
2025-01-09 18:17:52 -0300

    Feat: plus do edições e excluir colocados

--- projeto novo/js/utils/template.js
+++ projeto novo/js/utils/template.js
@@ -4,6 +4,7 @@ const itemCarrosselFilmes = `
         <div class="container-item-img">
             <img src="{img}" alt="Imagem do item" class="item-img" />
             <div class="capa">
+            <button id="plusModal" ><img src="../../img/plus.svg" alt="plus" class="plus" style="cursor: pointer;"></button>
                 <p class="titulo">Titulo</p>
                 <p class="generos">Aventura, Fantasia</p>
                 <p class="ano-lancamento">2007</p>
